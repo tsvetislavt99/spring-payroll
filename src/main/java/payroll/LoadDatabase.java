19c3c011f8b1a3c3152ede658f21e5db6c847647
Tsvetislav Todorov <tsvetislav.todorov@mentormate.com>
2022-10-31 11:10:40 +0200

    Update mock data in LoadDatabse

--- src/main/java/payroll/LoadDatabase.java
+++ src/main/java/payroll/LoadDatabase.java
@@ -8,14 +8,26 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class LoadDatabase {
-    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-    @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository) {
-        return args -> {
-            log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar")));
-            log.info("Preloading " + repository.save(new Employee("Frodo", "Baggins", "thief")));
-        };
-    }
-    
+  private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+  @Bean
+  CommandLineRunner initDatabase(EmployeeRepository employeeRepository, OrderRepository orderRepository) {
+
+    return args -> {
+      employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar"));
+      employeeRepository.save(new Employee("Frodo", "Baggins", "thief"));
+
+      employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
+
+      
+      orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+      orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+
+      orderRepository.findAll().forEach(order -> {
+        log.info("Preloaded " + order);
+      });
+      
+    };
+  }
 }
